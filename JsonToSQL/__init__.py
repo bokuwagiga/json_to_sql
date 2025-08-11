14d3594ca2a61df334133a2be0f8157f04c453d9
bokuwagiga <giga.shubitidze.1@btu.edu.ge>
2025-08-11 19:44:10 +0300

    update init files

--- JsonToSQL/__init__.py
+++ JsonToSQL/__init__.py
@@ -1,10 +1,14 @@
-from JsonToSQL.database.sql_writer import SqlServerTableCreator
-from JsonToSQL.main.json_to_sql import process_json_to_sql_server
-from JsonToSQL.core.normalizer import JsonNormalizer
-from JsonToSQL.core.analyzer import JsonStructureAnalyzer
-from JsonToSQL.core.table_builder import TableBuilder
+from .core.normalizer import JsonNormalizer
+from .core.analyzer import JsonStructureAnalyzer
+from .core.table_builder import TableBuilder
+from .database.sql_writer import SqlServerTableCreator
+
+from .main.json_to_sql import process_json_to_sql_server
 
 __all__ = [
-    "SqlServerTableCreator", "process_json_to_sql_server", "JsonNormalizer",
-    "JsonStructureAnalyzer", "TableBuilder",
+    "SqlServerTableCreator", 
+    "process_json_to_sql_server", 
+    "JsonNormalizer",
+    "JsonStructureAnalyzer", 
+    "TableBuilder",
 ]
