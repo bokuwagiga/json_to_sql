009d8114c035e4c5b52c82477c85cf0a78b64f29
bokuwagiga <giga.shubitidze.1@btu.edu.ge>
2025-08-11 19:31:36 +0300

    fix importing

--- JsonToSQL/__init__.py
+++ JsonToSQL/__init__.py
@@ -1,8 +1,8 @@
-from database.sql_writer import SqlServerTableCreator
-from main.json_to_sql import process_json_to_sql_server
-from core.normalizer import JsonNormalizer
-from core.analyzer import JsonStructureAnalyzer
-from core.table_builder import TableBuilder
+from JsonToSQL.database.sql_writer import SqlServerTableCreator
+from JsonToSQL.main.json_to_sql import process_json_to_sql_server
+from JsonToSQL.core.normalizer import JsonNormalizer
+from JsonToSQL.core.analyzer import JsonStructureAnalyzer
+from JsonToSQL.core.table_builder import TableBuilder
 
 __all__ = [
     "SqlServerTableCreator", "process_json_to_sql_server", "JsonNormalizer",
